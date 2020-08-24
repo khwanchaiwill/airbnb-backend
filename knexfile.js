a141a31b39beae947684ade7ca9f31b954490b22
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-08-24 01:02:06 -0400

    try to fix the error

--- knexfile.js
+++ knexfile.js
@@ -47,8 +47,11 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   }
 
 };
