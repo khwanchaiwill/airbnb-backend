f4b27a8a2ea010039bb31e6eb4a3132057ecf64a
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-08-24 08:12:06 -0400

    fixing the error

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 require("dotenv").config();
 
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+const {pgConnection} = require('./secretToken')
 module.exports = {
 
   development: {
@@ -23,21 +23,21 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
     client: "pg",
--- secretToken.js
+++ secretToken.js
@@ -1,3 +1,6 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECCRET || "This is very secret data",
+    pgConnection:
+    process.env.DATABASE_URL || "postgresql://postgres@localhost/auth",
+  rounds: parseInt(process.env.BCRYPT_ROUNDS) || 12,
 }
