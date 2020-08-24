2352ce447583ae2f3354b744822b6d882e94f854
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-08-23 23:20:40 -0400

    api up

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const server = require('./api/server.js')
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5432;
 
 server.listen(port, () => console.log(`listening to ${port}`));
